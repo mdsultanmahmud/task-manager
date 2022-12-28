135073e7c3d0ba495178a9c4f7a301911c707b34
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-12-28 16:40:17 +0600

    added navbar

--- src/sharedPages/Navbar.jsx
+++ src/sharedPages/Navbar.jsx
@@ -1,22 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
     const [menulist, setmenuList] = useState(false)
+    const [theme, setTheme] = useState('light')
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    }, [theme])
+    const handleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+    }
     return (
 
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
                 <Link to={'/'} className="flex items-center">
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Task Manager</span>
+                    <span className="self-center text-xl font-semibold whitespace-nowrap text-blue-600 dark:text-white">Task Manager</span>
                 </Link>
                 <div className="flex md:order-2">
                     <Link to={'/login'}><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button></Link>
                     <button onClick={() => setmenuList(!menulist)} data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div className={`items-center justify-between  ${!menulist? 'hidden':'block'} w-full md:flex md:w-auto md:order-1`} id="navbar-cta">
+                <div className={`items-center justify-between  ${!menulist ? 'hidden' : 'block'} w-full md:flex md:w-auto md:order-1`} id="navbar-cta">
                     <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <Link to={'/'} href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Add Task</Link>
@@ -29,6 +39,15 @@ const Navbar = () => {
                         </li>
                         <li>
                             <Link to={'/register'} href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Register</Link>
+                        </li>
+                        <li>
+                            <Link to={'/theme'} href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">theme</Link>
+                        </li>
+                        <li>
+                        <label  className="inline-flex relative items-center cursor-pointer">
+                            <input type="checkbox" value="" className="sr-only peer" onChange={handleTheme}/>
+                                <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                        </label>
                         </li>
                     </ul>
                 </div>
